879944a919105a1b3f9fc1b0f98da9093ff62e62
joshabbey <45700422+joshabbey@users.noreply.github.com>
2018-12-08 17:05:57 +1100

    Update dql-maze1.py
    agent class

--- dql-maze1.py
+++ dql-maze1.py
@@ -17,13 +17,16 @@
 # imports
 import numpy as np
 
+# Create maze class
 
-class maze(self, int height, int width):
-    def __init__(self, height, width):
+class maze(self, int height, int width,int goal, int mines):
+    def __init__(self, height, width, goal, mines):
         self.h = height
         self.w = width
         self.size = [height,width]
         self.maze = np.zeros((height,width))
+        self.goal = goal
+        self.mines = mines
     
     def updateMaze(self,int y, int x, str action):
         
@@ -35,14 +38,14 @@ class maze(self, int height, int width):
                 y = y-1
         
         if action == 'right':
-            if x =! self.width or self.maze[y][x+1] != 9:
+            if x =! self.w or self.maze[y][x+1] != 9:
                 self.maze[y][x] =0
                 self.maze[y][x+1] = 1
                 
                 x = x+1
                 
         if action == 'down':
-            if y =! self.height or self.maze[y + 1][x] != 9:
+            if y =! self.h or self.maze[y + 1][x] != 9:
                 self.maze[y][x] =0
                 self.maze[y+1][x] = 1
                 
@@ -57,7 +60,54 @@ class maze(self, int height, int width):
                 
         return y,x
     
-    def getReward(self,int state[], int old_state[],)
+    def getReward(self,int state):
+        if state == self.goal:
+            return 100 # goal found
+        else if state in self.mines:
+            return -100 # landed on a mine
+        else:
+            return -0.1 # time penalty
+
+class agent(self, str actions, length_input_layer, length_hidden_layer,length_output_layer, discount,epsilon,learning_rate,replace_target_iteration):
+    
+    # Create agent neural network
+    def __init__(self):    
+        self.input_layer = np.zeros((length_input_layer,1))
+        self.weights1 = np.random.normal(0,1,(length_hidden_layer,length_input_layer))
+        self.biases1 = np.random.normal(0,1,(1,length_hidden_layer))
+        self.hidden_layer = np.zeros((1,length_hidden_layer))
+        self.weights2 = np.random.normal(0,1,(length_output_layer,length_hidden_layer))
+        self.biases2 = np.random.normal(0,1,(length_output_layer,1))
+        self.output_layer = np.zeros((length_output_layer,1))
+        self.actions = actions
+        self.epsilon = episilon
+        self.target_weights1 = weights1
+        self.target_weights2 = weights2
+        self.memories = []
+        self.learning_rate = learning rate
+        self.replace_target_iteration = replace_target_iteration
+        
+    def actionSelection(self):
+        
+        # e-greddy action selection policy
+        if np.random.random_sample() >= self.epsilon:        
+            return self.actions[argmax(self.output_layer)]
+        else:
+            return self.actions[np.random.randint(0,5)]
+        
+    def memoryStore(self,s,q,a,s1,r,q1):
+        self.memories.append([s,q,a,s1,r,q1])
+    
+    def qualityDelta(self)
+    
+        
+        
+        
+        
+        
+        
+        
+        
     
     
         
